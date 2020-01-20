de4870686fbf5ec3c587f28937bb5ba027b50bbf
lankmar <32324313+lankmar@users.noreply.github.com>
2020-01-20 18:34:26 +0300

    some change

--- HomeWork_1/Program.cs
+++ HomeWork_1/Program.cs
@@ -7,16 +7,18 @@ namespace HomeWork_1
     {
         static void Main(string[] args)
         {
-            //Store store = new Store(new DefaultIdGererator());
+           //задание 1
+            Store store = new Store(new DefaultIdGererator());
 
-            //Console.WriteLine(store.Id.ToString());
-            //Console.ReadLine();
+            Console.WriteLine(store.Id.ToString());
+            Console.ReadLine();
+
+            //задание 2
             IEnumerable<Func<string>> funcs = Task2.GetActionStep();
             foreach (var item in funcs)
             {
                 Task2.MakeFunc(item);
             }
-
 
             Console.ReadLine();
         }
