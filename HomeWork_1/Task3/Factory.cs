f46b8e3976b5ed264212ed8159349d652d1b426c
lankmar <32324313+lankmar@users.noreply.github.com>
2020-01-28 12:17:52 +0300

    Home work 3
    Home work to lesson 3

---
+++ HomeWork_1/Task3/Factory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_1.Task3
+{
+    public class Factory
+    {
+        public static IFigure GetFigure(string figureType)
+        {
+            if (figureType == null) return null;
+
+            switch (figureType)
+            {
+                
+                case "Треугольник":
+                    return new Triangle();
+                case "Квадрат":
+                    return new Square();
+                case "Прямоугольник":
+                    return new Rectangle();
+                default:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HomeWork_1/Task3/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeWork_1.Task3
+{
+    public class Square : IFigure
+    {
+
+        public void Draw()
+        {
+            System.Console.WriteLine("Квадрат");
+        }
+
+    }
+}
