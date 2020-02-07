a170e794e7d9833b8f16966a8a729fe4110e214e
lankmar <32324313+lankmar@users.noreply.github.com>
2020-02-07 18:32:48 +0300

    Home work 6
    Home work 6

---
+++ HomeWork_1/06/ApprovalResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_1._06
+{
+    public class AnswerApproval 
+    {
+        public AnswerApproval nextApprovalResponse;
+
+        public static AnswerApproval BeyondApprovalLimit { get; set; }
+        public static AnswerApproval Approve { get; set; }
+
+        internal static AnswerApproval Denied;
+
+        public AnswerApproval(IExpenseRequest expense)
+        { 
+        
+        } 
+        public AnswerApproval()
+        {
+        }
+
+         public AnswerApproval ApprovalExpense(IExpenseRequest expenseReport)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+ 
+}
---
+++ HomeWork_1/06/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork_1._06
+{
+    public class Employee : IExpenseConfirmatoin
+    {
+        public string Name { get; private set; }
+        private double _approvalLimit;
+
+        public Employee(string name, double approvalLimit)
+        {
+            Name = name;
+            _approvalLimit = approvalLimit;
+        }
+
+        public AnswerApproval Expense(IExpenseRequest expenseReport)
+        {
+            if (expenseReport.Total > _approvalLimit)
+            {
+                return AnswerApproval.BeyondApprovalLimit;
+            }
+            return   AnswerApproval.Approve;
+        }
+    }
+}
