1c3f6101b0bb053df65b4dca5e5a1156ba4f1cbd
lankmar <32324313+lankmar@users.noreply.github.com>
2020-02-10 19:28:08 +0300

    Home work 7
    Home work 7

---
+++ HomeWork_1/07/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_1._07
+{
+    public class Address
+    {
+        public string ContactName { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+
+    public enum ShippingOptions
+    { 
+        UFS = 1,
+        FedEx,
+        EMS
+    }
+
+    public class Order
+    {
+        public ShippingOptions ShippingMethod { get; set; }
+        public Address Destination { get; set; }
+        public Address Origin { get; set; }
+    }
+}
---
+++ HomeWork_1/07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+//1. Реализовать шаблон «Стратегия» без использования делегатов. 
+//При решении задачи следует применить следующий интерфейс:
+//public interface IShippingStrategy
+//{
+//    double Calculate(Order order);
+//}
+
+namespace HomeWork_1._07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShippingStrategy ems = new EMSShippingStrategy();
+            IShippingStrategy fedex = new FedExShippingStrategy();
+            IShippingStrategy ufs = new UFSShippingStrategy();
+
+            var costCalculation = new CostCalculation(ems).ShippingCostCalculation(new Order());
+
+            Console.ReadLine();
+        }
+    }
+}
